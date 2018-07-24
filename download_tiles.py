e4f292b67af8a627a10414c4b18e63ac7b6c9569
高江宁 <gjn_2012@foxmail.com>
2018-07-24 09:31:09 +0800

    添加注释、下载参数修改

--- download_tiles.py
+++ download_tiles.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python
+"""
+    说明：
+    1、akey若无法使用，请在gmap_utils.py文件中修改自己的akey
 
+    2、本文件为下载的入口，其中函数download_tile、download_satellite里定义的的链接地址中的参数
+    udt需要修改为最新版本的时间字符串，例如"20180628"，
+
+    3、时间字符串获取方法：
+    浏览器访问：http://api.map.baidu.com/api?v=2.0，获取到js中document.write中的script标签的src，
+    再浏览器访问该src，获取返回的js脚本中全局变量TILE_VERSION对象中的'updateDate'属性即可，一般在第一行就能找到
+
+    4、服务端替换离线瓦片资源时，不要忘记修改离线BaiduApi_2.0.js文件设置或者js中的window.__BMAP_EXTRA_CONFIG__
+    中的各种Udt参数，要与图片的udt保持一致
+"""
 import urllib.request, urllib.error, urllib.parse
 from threading import Thread
 import os, sys
@@ -9,19 +22,19 @@ from gmap_utils import *
 import time
 import random
 
-def download_tiles(zoom, lat_start, lat_stop, lon_start, lon_stop, satellite=True):
 
+def download_tiles(zoom, lat_start, lat_stop, lon_start, lon_stop, satellite=True):
     start_x, start_y = bd_latlng2xy(zoom, lat_start, lon_start)
     stop_x, stop_y = bd_latlng2xy(zoom, lat_stop, lon_stop)
-    
-    start_x = int(start_x//256)
-    start_y = int(start_y//256)
-    stop_x = int(stop_x//256)
-    stop_y = int(stop_y//256)
-    
+
+    start_x = int(start_x // 256)
+    start_y = int(start_y // 256)
+    stop_x = int(stop_x // 256)
+    stop_y = int(stop_y // 256)
+
     print("x range", start_x, stop_x)
     print("y range", start_y, stop_y)
-    
+
     for x in range(start_x, stop_x):
         # download(x, y, zoom)
         FastThread(x, start_y, stop_y, zoom, satellite).start()
@@ -35,7 +48,7 @@ class FastThread(Thread):
         self.stop_y = stop_y
         self.zoom = zoom
         self.satellite = satellite
-    
+
     def run(self):
         for y in range(self.start_y, self.stop_y):
             if satellite:
@@ -53,7 +66,7 @@ def download_tile(x, y, zoom, satellite=False):
     # styles is roadmap when downloading satellite
     styles = "sl" if satellite else "pl"
 
-    query = "qt=tile&x=%d&y=%d&z=%d&styles=%s%s&udt=20170927" % (x, y, zoom, styles, scaler)
+    query = "qt=tile&x=%d&y=%d&z=%d&styles=%s%s&udt=20180628" % (x, y, zoom, styles, scaler)  # 修改时间字符串(udt=后面的)
     url = "http://online0.map.bdimg.com/onlinelabel/?" + query
     filename = query + ".png"
 
@@ -65,7 +78,7 @@ def download_satellite(x, y, zoom):
     filename = None
     folder = "it/"
 
-    path = "u=x=%d;y=%d;z=%d;v=009;type=sate&fm=46&udt=20170927" % (x, y, zoom)
+    path = "u=x=%d;y=%d;z=%d;v=009;type=sate&fm=46&udt=20180628" % (x, y, zoom)  # 修改时间字符串(udt=后面的)
     url = "http://shangetu0.map.bdimg.com/it/" + path
     filename = path.replace(";", ",") + ".jpg"
 
@@ -83,29 +96,28 @@ def download_file(url, filename, folder=""):
         except Exception as e:
             print("--", filename, "->", e)
             sys.exit(1)
-        
+
         if bytes.startswith(b"<html>"):
             print("-- forbidden", filename)
             sys.exit(1)
-        
+
         print("-- saving " + filename)
-        
+
         f = open(full_file_path, 'wb')
         f.write(bytes)
         f.close()
-        
+
         time.sleep(1 + random.random())
     else:
         print("-- existed " + filename)
-            
+
 
 if __name__ == "__main__":
-    
-    zoom = 19
- 
-    lat_start, lon_start = 34.233188,108.91931,
-    lat_stop, lon_stop = 34.241843,108.928293,
+    zoom = 11
 
-    satellite = True
-	
+    lat_start, lon_start = 36, 115  # 116.419791,36.871881
+    lat_stop, lon_stop = 37.5, 119  # 117.04185,36.741968
+
+    satellite = False  # 是否下载卫星图/街道
+
     download_tiles(zoom, lat_start, lat_stop, lon_start, lon_stop, satellite)
--- gmap_utils.py
+++ gmap_utils.py
@@ -1,45 +1,56 @@
 # http://oregonarc.com/2011/02/command-line-tile-cutter-for-google-maps-improved/
 # http://media.oregonarc.com/fish/tile.py
-
+"""
+    说明：
+    akey若无法使用，请修改自己的akey，目前为我个人申请的akey
+"""
 import math
 import urllib.request, urllib.parse, urllib.error
 import urllib.request, urllib.error, urllib.parse
 import json
 
-akey = 'uzIatbtHgfCTr71dWxnHolfZqG6vARNc'
+akey = 'GPhyLRdlS8DfNadsLEwY3iE2H1WoQTVL'
 
-def latlon2px(z,lat,lon):
-    x = 2**z*(lon+180)/360*256
-    y = -(.5*math.log((1+math.sin(math.radians(lat)))/(1-math.sin(math.radians(lat))))/math.pi-1)*256*2**(z-1)
-    return x,y
 
-def latlon2xy(z,lat,lon):
-    x,y = latlon2px(z,lat,lon)
-    x = int(x/256)#,int(x%256)
-    y = int(y/256)#,int(y%256)
-    return x,y
+def latlon2px(z, lat, lon):
+    x = 2 ** z * (lon + 180) / 360 * 256
+    y = -(
+        .5 * math.log(
+            (1 + math.sin(math.radians(lat))) / (1 - math.sin(math.radians(lat)))) / math.pi - 1) * 256 * 2 ** (
+        z - 1)
+    return x, y
 
-def bd_latlng2xy(z,lat,lng):
-    url='http://api.map.baidu.com/geoconv/v1/?'
-    args = {'coords':str(lng)+','+str(lat),
-            'from':5,
-            'to':6,
-            'output':'json',
-            'ak':akey}
+
+def latlon2xy(z, lat, lon):
+    x, y = latlon2px(z, lat, lon)
+    x = int(x / 256)  # ,int(x%256)
+    y = int(y / 256)  # ,int(y%256)
+    return x, y
+
+
+def bd_latlng2xy(z, lat, lng):
+    url = 'http://api.map.baidu.com/geoconv/v1/?'
+    args = {'coords': str(lng) + ',' + str(lat),
+            'from': 5,
+            'to': 6,
+            'output': 'json',
+            'ak': akey}
     data = urllib.parse.urlencode(args)
-    response = urllib.request.urlopen(url+data)
+    response = urllib.request.urlopen(url + data)
     result = response.read()
     result = json.loads(result)
+    print(result)
     loc = result["result"][0]
-    res = 2**(18-z)
-    x = loc['x']/res
-    y = loc['y']/res
-    return x,y
+    res = 2 ** (18 - z)
+    x = loc['x'] / res
+    y = loc['y'] / res
+    return x, y
+
 
 if __name__ == "__main__":
-    z=19
-    lat=31.025819
-    lng=121.434229
-    x,y = bd_latlng2xy(z,lat,lng)
-    print(x//256)
-    print(y//256) # only right when lat>0 lng>0
+    z = 19
+    lat = 31.025819
+    lng = 121.434229
+    x, y = bd_latlng2xy(z, lat, lng)
+    print(x // 256)
+    print(y // 256)  # only right when lat>0 lng>0
